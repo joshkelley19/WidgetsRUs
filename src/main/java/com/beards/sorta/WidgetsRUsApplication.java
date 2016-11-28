857cde4240749271344bcc92a4673ba7baeff3cd
joshuakelley <joshkelley19@gmail.com>
2016-11-28 11:12:26 -0500

    initial commit

---
+++ src/main/java/com/beards/sorta/WidgetsRUsApplication.java
@@ -0,0 +1,12 @@
+package com.beards.sorta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetsRUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetsRUsApplication.class, args);
+	}
+}
